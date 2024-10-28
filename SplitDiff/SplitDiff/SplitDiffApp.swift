61c92d8698ff6fd5f2abe8ddc047775f2db03b9e
Phuykong <m.phuykong@gmail.com>
2024-10-28 14:44:21 -0400

    init version

---
+++ SplitDiff/SplitDiff/SplitDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitDiffApp.swift
+//  SplitDiff
+//
+//  Created by Phuykong on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SplitDiff/SplitDiffTests/SplitDiffTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitDiffTests.swift
+//  SplitDiffTests
+//
+//  Created by Phuykong on 10/28/24.
+//
+
+import Testing
+@testable import SplitDiff
+
+struct SplitDiffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
