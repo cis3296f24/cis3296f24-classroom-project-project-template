f459891599c30f51a884ce9f60b6583979b1a6ac
Jiacheng He <tuo44998@temple.edu>
2024-10-30 13:59:48 -0400

    added several packages and modified the setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 
