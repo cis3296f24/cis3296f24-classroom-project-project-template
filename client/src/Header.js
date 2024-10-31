658391a3e119dc2c88adf75c62aa930e9080ffef
Jiacheng He <tuo44998@temple.edu>
2024-10-30 22:06:39 -0400

    modified navBar

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,129 +1,110 @@
-'use client'
-
+import { Dialog, DialogPanel } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import Lottie from "lottie-react";
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
-} from '@headlessui/react';
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline';
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
-
-const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-];
-
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
+import { NavLink } from 'react-router-dom';
+import pixelAnimation from "./assets/pixel.json";
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+    <header className="bg-transparent">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8">
         <div className="flex lg:flex-1">
-          <Link to="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
+          <NavLink to="/" className="pixelify-sans-bold text-lg">
+            <Lottie
+              animationData={pixelAnimation}
+              loop={true}
+              style={{
+                position: "relative",
+                top: "0px",
+                left: "-15px",
+                width: "40px",
+                height: "40px",
+                display: "inline-block",
+              }}
             />
-          </Link>
+            <span style={{ position: "relative", top: "-10px" }}>GRINDDAILY</span>
+          </NavLink>
         </div>
         <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-          </button>
+        {mobileMenuOpen ? (
+            <button
+              type="button"
+              aria-label="Close menu"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5"
+            >
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          ) : (
+            <button
+              type="button"
+              aria-label="Open main menu"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+            >
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          )}
         </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <Link to="/" className="text-sm/6 font-semibold text-gray-900">
-            Home
-          </Link>
-          <Link to="/task" className="text-sm/6 font-semibold text-gray-900">
-            Task
-          </Link>
-          <Link to="/marketplace" className="text-sm/6 font-semibold text-gray-900">
-            Marketplace
-          </Link>
-          <Link to="/company" className="text-sm/6 font-semibold text-gray-900">
-            Company
-          </Link>
-        </PopoverGroup>
+        <div className="hidden lg:flex lg:gap-x-12">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => 
+            isActive ? "text-sm font-semibold text-indigo-400" : "text-sm font-semibold text-link"
+          }
+        >
+          Home
+        </NavLink>
+
+        <NavLink 
+          to="/task" 
+          className={({ isActive }) => 
+            isActive ? "text-sm font-semibold text-indigo-400" : "text-sm font-semibold text-link"
+          }
+        >
+          Task
+        </NavLink>
+        </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link to="/login" className="text-sm/6 font-semibold text-gray-900">
+          <NavLink to="/login" className="text-sm font-semibold text-link">
             Log in <span aria-hidden="true">&rarr;</span>
-          </Link>
+          </NavLink>
         </div>
       </nav>
+
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel 
+        style= {{ backgroundColor: 'var(--custom-background-color)' }}
+        className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-transparent px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link to="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
-            </Link>
+            <NavLink to="/" onClick={() => setMobileMenuOpen(false)} className="pixelify-sans-bold">
+              GRINDDAILY
+            </NavLink>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5"
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">
+                <NavLink to="/" onClick={() => setMobileMenuOpen(false)} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-link hover:bg-gray-700">
                   Home
-                </Link>
-                <Link to="/task" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">
+                </NavLink>
+                <NavLink to="/task" onClick={() => setMobileMenuOpen(false)} className="-mx-3 block rounded-lg px-3 py-2 text-base text-link font-semibold hover:bg-gray-700">
                   Task
-                </Link>
-                <Link to="/marketplace" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">
-                  Marketplace
-                </Link>
-                <Link to="/company" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">
-                  Company
-                </Link>
+                </NavLink>
               </div>
               <div className="py-6">
-                <Link to="/login" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold text-gray-900 hover:bg-gray-50">
+                <NavLink to="/login" onClick={() => setMobileMenuOpen(false)} className="-mx-3 block rounded-lg px-3 py-2.5 text-base text-link font-semibold hover:bg-gray-700">
                   Log in
-                </Link>
+                </NavLink>
               </div>
             </div>
           </div>
