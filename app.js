e16f9fb9c42c1beb0116b4ec1d73dc16f7821c55
Larry Banks <larrykbanks21@gmail.com>
2024-11-06 16:44:42 -0500

    removed unnecessary line

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const path = require('path');
 
 const hostname = 'localhost';
 const port = '9999';
+
 app.use(express.static(path.join(__dirname, 'static')));
-app.set('');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
