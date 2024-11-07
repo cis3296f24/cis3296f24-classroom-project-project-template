70c2f23113638227b68a20acbc77646a05d542fb
Ethan <tuq08268@temple.edu>
2024-11-07 18:30:02 -0500

    made planets change size based on listens

--- public/main.js
+++ public/main.js
@@ -111,7 +111,7 @@ function renderTracks(data) {
         .range([height - 50, 50]);
 
     const minSize = 5;
-    const maxSize = 5;
+    const maxSize = 100;
 
     svg.selectAll("circle")
         .data(topArtists)
@@ -120,7 +120,7 @@ function renderTracks(data) {
         .attr("cx", d => xScale(d.value.avgPopularity))
         .attr("cy", d => yScale(d.value.avgDuration))
         .attr("r", d => {
-            const radius = minSize + ((d.value.avgPopularity / 100) * (maxSize - minSize));
+            const radius = minSize + ((d.value.count / 10) * (maxSize - minSize));
             return radius;
         })
         .attr("fill", (d, i) => planetColors[i % planetColors.length])
