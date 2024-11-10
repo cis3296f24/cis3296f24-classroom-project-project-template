23fb072cc34d1c13f5e505568e128ad526933d6e
KYWILS21 <tuk30460@temple.edu>
2024-11-10 16:38:38 -0500

    Organized Dev Team

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -62,51 +62,57 @@ function Home() {
 
             <Bar title="Meet Our Dev Team"></Bar>
 
-            <div>
-
-                <div>
-                    <h3 className='px-2 text-gray-500 text-lg font-semibold'>Kyle Wilson</h3>
-                            <ul>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Email: tuk30460@temple.edu</li>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Phone #: 215-###-#####</li>
-                            </ul>
-                </div>
-
-                <div>
-                    <h3 className='px-2 text-gray-500 text-lg font-semibold'>Antonio Mongeluzi</h3>
-                            <ul>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Email: tuj46268@temple.edu</li>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Phone #: 215-960-####</li>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Address: 1234 Temple University Rd</li>
-                            </ul>
-                </div>
-                
-                <div>
-                    <h3 className='px-2 text-gray-500 text-lg font-semibold'>Yousuf Qari</h3>
-                            <ul>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Email: tuo65470@temple.edu</li>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Phone #: 215-###-####</li>
-                            </ul>
-                </div>
-
-                <div>
-                    <h3 className='px-2 text-gray-500 text-lg font-semibold'>Jie Huang</h3>
-                            <ul>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Email: tuo77200@temple.edu</li>
-                                <li className='px-2 text-gray-500 text-lg font-semibold'>Phone #: 215-###-####</li>
-                            </ul>
-                </div>
-            </div>
-
-            <div>
-                <h3 className='px-2 text-gray-500 text-lg font-semibold'>Jie Huang</h3>
-                        <ul>
-                            <li className='px-2 text-gray-500 text-lg font-semibold'>Email: tuo77200@temple.edu</li>
-                            <li className='px-2 text-gray-500 text-lg font-semibold'>Phone #: 215-###-####</li>
-                        </ul>
-            </div>
-            
+            <div className="flex justify-center gap-6 mt-8 flex-wrap md:flex-nowrap w-full px-4">
+        <div className="text-center flex-1 min-w-[150px]">
+            <h3 className="text-gray-500 text-lg font-semibold">Peter Ly</h3>
+            <ul className="text-gray-500 text-md font-normal">
+                <li>Email:</li>
+                <li>Phone #: #</li>
+            </ul>
         </div>
+
+        <div className="text-center flex-1 min-w-[150px]">
+            <h3 className="text-gray-500 text-lg font-semibold">Harry He</h3>
+            <ul className="text-gray-500 text-md font-normal">
+                <li>Email:</li>
+                <li>Phone #:</li>
+            </ul>
+        </div>
+
+        <div className="text-center flex-1 min-w-[150px]">
+            <h3 className="text-gray-500 text-lg font-semibold">Jie Huang</h3>
+            <ul className="text-gray-500 text-md font-normal">
+                <li>Email: tuo77200@temple.edu</li>
+                <li>Phone #: 215-###-####</li>
+            </ul>
+        </div>
+
+        <div className="text-center flex-1 min-w-[150px]">
+            <h3 className="text-gray-500 text-lg font-semibold">Kyle Wilson</h3>
+            <ul className="text-gray-500 text-md font-normal">
+                <li>Email: tuk30460@temple.edu</li>
+                <li>Phone #: 215-###-#####</li>
+            </ul>
+        </div>
+
+        <div className="text-center flex-1 min-w-[150px]">
+            <h3 className="text-gray-500 text-lg font-semibold">Antonio Mongeluzi</h3>
+            <ul className="text-gray-500 text-md font-normal">
+                <li>Email: tuj46268@temple.edu</li>
+                <li>Phone #: 215-960-####</li>
+                <li>Address: 1234 Temple University Rd</li>
+            </ul>
+        </div>
+
+        <div className="text-center flex-1 min-w-[150px]">
+            <h3 className="text-gray-500 text-lg font-semibold">Yousuf Qari</h3>
+            <ul className="text-gray-500 text-md font-normal">
+                <li>Email: tuo65470@temple.edu</li>
+                <li>Phone #: 215-###-####</li>
+            </ul>
+        </div>
+    </div>
+</div>
     );
 }
 
