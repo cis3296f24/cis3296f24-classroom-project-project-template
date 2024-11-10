0a84f0e763de1d5397231bafe32e24f73a66a6bb
Shafiq Rahman <shafiq@temple.edu>
2024-11-10 15:31:48 -0500

    Integrated pressing spacebar
    tested space bar is working.

--- KeyboardInputs.java
+++ KeyboardInputs.java
@@ -6,13 +6,25 @@ public class KeyboardInputs implements KeyListener {
     public void keyTyped(KeyEvent e) {
         // todo
     }
-
     public void keyPressed(KeyEvent e) {
         System.out.println("Key pressed: ");
-    }
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_UP:
+            break;
+        case KeyEvent.VK_DOWN:
+            break;
+        case KeyEvent.VK_LEFT:
+            break;
+        case KeyEvent.VK_RIGHT:
+            break;
+        case KeyEvent.VK_SPACE:
+            System.out.println("Space");
+            break;
 
+        }
+    }
     public void keyReleased(KeyEvent e) {
-        // System.out.println("Key released");
+        // todo
     }
 
 }
