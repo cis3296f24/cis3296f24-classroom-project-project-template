f8fd1b8176922a14e1b5191d3ffc24b934e99077
Larry Banks <larrykbanks21@gmail.com>
2024-11-11 21:49:32 -0500

    refactored Home.svelte to use Trip Planner component

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import { Map } from 'maplibre-gl';
     import 'maplibre-gl/dist/maplibre-gl.css';
+    import TripPlanner from './TripPlanner.svelte';
     
     onMount(() => {
         //hack until i look into svelte
@@ -234,66 +235,7 @@
 </script>
 
 <main>
-    <div class="userInputBackground">
-        <form action="">
-            <input type="text" id="start" name="start" value="Start" /><br /><br
-            />
-            <input type="text" id="end" name="end" value="End" />
-            <input type="submit" value="Swap" /><br /><br />
-            <div class="inlineElements">
-                <div class="pairLabel">
-                    <input
-                        type="radio"
-                        id="leave"
-                        name="button"
-                        value="leave"
-                        checked
-                    />
-                    <label for="leave">Leave</label>
-                </div>
-                <div class="pairLabel">
-                    <input
-                        type="radio"
-                        id="arrive"
-                        name="button"
-                        value="arrive"
-                    />
-                    <label for="arrive">Arrive</label>
-                </div>
-                <a href="">+Round Trip</a>
-                <a href="">+Add Stop</a>
-            </div>
-            <br />
-            <input type="text" id="date" name="date" value="Today" /><br /><br
-            />
-            <input type="text" id="time" name="time" value="ASAP" /><br /><br />
-            <div class="inlineElements">
-                <div class="pairLabel">
-                    <input type="checkbox" id="bus" name="bus" checked />
-                    <label for="bus">Bus</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="checkbox" id="subway" name="subway" checked />
-                    <label for="subway">Subway</label>
-                </div>
-                <div class="pairLabel">
-                    <input
-                        type="checkbox"
-                        id="trolley"
-                        name="trolley"
-                        checked
-                    /><br />
-                    <label for="trolley">Trolley</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="checkbox" id="rail" name="rail" checked />
-                    <label for="rail">Rail</label>
-                </div>
-            </div>
-            <br />
-            <input type="submit" value="Go" />
-        </form>
-    </div>
+    <TripPlanner/>
     <div id="map" style="height: 500px; width: 100%;"></div>
     <button id="fetch-button">Fetch Locations</button>
     <div id="response-container"></div>
--- src/components/TripPlanner.svelte
+++ src/components/TripPlanner.svelte
@@ -1,70 +1,67 @@
 <script>
-    let el = document.getElementById("tp-submit")
-    console.log(el)
+    // import { onMount } from "svelte";
+    // Swap button
+
+    // Go button
+        const handleClick = (event) => {
+            // gather all data and store
+            event.preventDefault();
+            let start = document.getElementById("start")
+            let end = document.getElementById("end")
+            // validate input
+            console.log(`Start:${start.value}, End:${end.value}`)
+        };
 </script>
 
-<main>
-    <div class="userInputBackground">
-        <form action="">
-            <input type="text" id="start" name="start" value="Start" /><br /><br
-            />
-            <input type="text" id="end" name="end" value="End" />
-            <input type="submit" value="Swap" /><br /><br />
-            <div class="inlineElements">
-                <div class="pairLabel">
-                    <input
-                        type="radio"
-                        id="leave"
-                        name="button"
-                        value="leave"
-                        checked
-                    />
-                    <label for="leave">Leave</label>
-                </div>
-                <div class="pairLabel">
-                    <input
-                        type="radio"
-                        id="arrive"
-                        name="button"
-                        value="arrive"
-                    />
-                    <label for="arrive">Arrive</label>
-                </div>
-                <a href="">+Round Trip</a>
-                <a href="">+Add Stop</a>
+<div class="userInputBackground">
+    <form action="">
+        <input type="text" id="start" name="start" placeholder="Start" /><br /><br />
+        <input type="text" id="end" name="end" placeholder="End" />
+        <input type="submit" value="Swap" /><br /><br />
+        <div class="inlineElements">
+            <div class="pairLabel">
+                <input
+                    type="radio"
+                    id="leave"
+                    name="button"
+                    value="leave"
+                    checked
+                />
+                <label for="leave">Leave</label>
             </div>
-            <br />
-            <input type="text" id="date" name="date" value="Today" /><br /><br
-            />
-            <input type="text" id="time" name="time" value="ASAP" /><br /><br />
-            <div class="inlineElements">
-                <div class="pairLabel">
-                    <input type="checkbox" id="bus" name="bus" checked />
-                    <label for="bus">Bus</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="checkbox" id="subway" name="subway" checked />
-                    <label for="subway">Subway</label>
-                </div>
-                <div class="pairLabel">
-                    <input
-                        type="checkbox"
-                        id="trolley"
-                        name="trolley"
-                        checked
-                    /><br />
-                    <label for="trolley">Trolley</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="checkbox" id="rail" name="rail" checked />
-                    <label for="rail">Rail</label>
-                </div>
+            <div class="pairLabel">
+                <input type="radio" id="arrive" name="button" value="arrive" />
+                <label for="arrive">Arrive</label>
             </div>
-            <br />
-            <input type="submit" value="Go" id="tp-submit" />
-        </form>
-    </div>
-</main>
+            <a href="">+Round Trip</a>
+            <a href="">+Add Stop</a>
+        </div>
+        <br />
+        <input type="text" id="date" name="date" placeholder="Today" /><br /><br />
+        <input type="text" id="time" name="time" placeholder="at" /><br /><br />
+        <div class="inlineElements">
+            <div class="pairLabel">
+                <input type="checkbox" id="bus" name="bus" checked />
+                <label for="bus">Bus</label>
+            </div>
+            <div class="pairLabel">
+                <input type="checkbox" id="subway" name="subway" checked />
+                <label for="subway">Subway</label>
+            </div>
+            <div class="pairLabel">
+                <input type="checkbox" id="trolley" name="trolley" checked /><br
+                />
+                <label for="trolley">Trolley</label>
+            </div>
+            <div class="pairLabel">
+                <input type="checkbox" id="rail" name="rail" checked />
+                <label for="rail">Rail</label>
+            </div>
+        </div>
+        <br />
+        <input type="submit" value="Go" id="tp-submit" on:click={handleClick} />
+    </form>
+</div>
 
 <style>
     .userInputBackground {
