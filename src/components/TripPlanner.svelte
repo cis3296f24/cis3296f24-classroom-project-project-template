b715832963b7f3e5689d9d7e5a29946a8602c36e
Larry Banks <larrykbanks21@gmail.com>
2024-11-14 08:21:29 -0500

    updated createTrip() to send requests to backend

--- src/components/TripPlanner.svelte
+++ src/components/TripPlanner.svelte
@@ -1,27 +1,23 @@
 <script>
-    // import { onMount } from "svelte";
-    let radio = "leave"
-    let bus = true
-    let subway  = true
-    let trolley = true
-    let rail    = true
-    
-    import { convertFilter } from "maplibre-gl";
+    let radio = "leave";
+    let bus = true;
+    let subway = true;
+    let trolley = true;
+    let rail = true;
 
-    // import { onMount } from "svelte";
     // Auto suggest
     const suggestLocation = (event) => {
-        console.log(`Pressed ${event.data}`)
+        console.log(`Pressed ${event.data}`);
     };
     // Swap button
     const handleSwap = (event) => {
-        let start = document.getElementById("start")
-        let end = document.getElementById("end")
-        console.log(`Before: start: ${start.value}, end: ${end.value}`)
-        let tmp = end.value
-        end.value = start.value
-        start.value = tmp
-        console.log(`After: start: ${start.value}, end: ${end.value}`)
+        let start = document.getElementById("start");
+        let end = document.getElementById("end");
+        console.log(`Before: start: ${start.value}, end: ${end.value}`);
+        let tmp = end.value;
+        end.value = start.value;
+        start.value = tmp;
+        console.log(`After: start: ${start.value}, end: ${end.value}`);
         console.log("Swap Button Clicked!");
     };
     // Go button
@@ -34,7 +30,7 @@
         let time = document.getElementById("time");
         // validate input
         // refactor html to just use a calendar for date and create a MM/DD format for time
-        let data = {
+        const data = {
             start: start.value,
             end: end.value,
             radio: radio,
@@ -45,56 +41,104 @@
             trolley: trolley,
             rail: rail,
         };
-        // send data to some function
+        // send data to trip creation function
         createTrip(data);
     };
 
     // create potential trips
-    function createTrip(data) {
-        // // convert locations to coordinates
-        // coords = getCoords()
-        // // pass to function to get trips
-        // routes = getTrips()
-        // // write to DOM
+    async function createTrip(data){
+        console.log(JSON.stringify(data))
+        const res = await fetch("/api/google_directions", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+        });
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status} `);
+        }
+        // routes = await req.json();
+        // write to DOM
         // displayTrips()
-        console.log(`Creating Trip!!! Payload: ${JSON.stringify(data)}`);
+        // console.log(`Creating Trip Payload: ${JSON.stringify(res)}`);
     }
+        
+        
 </script>
 
 <div class="userInputBackground">
     <form action="">
-        <input type="text" id="start" name="start" placeholder="Start" on:input={suggestLocation} />
-        <input type="text" id="end" name="end" placeholder="End" />
+        <input
+            type="text"
+            id="start"
+            name="start"
+            placeholder="Start"
+            autocomplete="off"
+            on:input={suggestLocation}
+        />
+        <input type="text" id="end" name="end" placeholder="End" autocomplete="off" />
         <button id="swap" type="button" on:click={handleSwap}>Swap</button>
         <div class="inlineElements">
             <label>
-                <input type="radio" id="leave" name="button" value="leave" bind:group={radio} /> Leave
+                <input
+                    type="radio"
+                    id="leave"
+                    name="button"
+                    value="leave"
+                    bind:group={radio}
+                /> Leave
             </label>
             <label>
-                <input type="radio" id="arrive" name="button" value="arrive" bind:group={radio} /> Arrive
+                <input
+                    type="radio"
+                    id="arrive"
+                    name="button"
+                    value="arrive"
+                    bind:group={radio}
+                /> Arrive
             </label>
             <a href="">+Round Trip</a>
             <a href="">+Add Stop</a>
         </div>
-        <br />
-        <input type="text" id="date" name="date" placeholder="Today" /><br/><br/>
-        <input type="text" id="time" name="time" placeholder="At" /><br/><br/>
+        <input type="date" id="date" name="date" placeholder="Today" />
+        <input type="time" id="time" name="time" placeholder="At" />
         <div class="inlineElements">
             <label>
-                <input type="checkbox" id="bus" name="bus" bind:checked={bus} /> Bus
+                <input type="checkbox" id="bus" name="bus" bind:checked={bus} />
+                Bus
             </label>
             <label>
-                <input type="checkbox" id="subway" name="subway" bind:checked={subway} /> Subway
+                <input
+                    type="checkbox"
+                    id="subway"
+                    name="subway"
+                    bind:checked={subway}
+                /> Subway
             </label>
             <label>
-                <input type="checkbox" id="trolley" name="trolley" bind:checked={trolley} /> Trolley
+                <input
+                    type="checkbox"
+                    id="trolley"
+                    name="trolley"
+                    bind:checked={trolley}
+                /> Trolley
             </label>
             <label>
-                <input type="checkbox" id="rail" name="rail" bind:checked={rail} /> Rail
+                <input
+                    type="checkbox"
+                    id="rail"
+                    name="rail"
+                    bind:checked={rail}
+                /> Rail
             </label>
         </div>
-        <br/>
-        <input type="submit" value="Go" id="tp-submit" on:click={handleFormSubmit} />
+        <input
+            type="submit"
+            value="Go"
+            id="tp-submit"
+            on:click={handleFormSubmit}
+        />
     </form>
 </div>
 
@@ -112,11 +156,11 @@
         align-items: center;
     }
 
-    .pairLabel {
+    /* .pairLabel {
         display: flex;
         flex-direction: row;
         justify-content: center;
         align-items: center;
         text-align: right;
-    }
+    } */
 </style>
