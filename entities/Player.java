80d1665ac63ead5724e37041122b65ad3dcf3c2d
Shafiq Rahman <shafiq@temple.edu>
2024-11-14 02:03:20 -0500

    Modify

--- entities/Player.java
+++ entities/Player.java
@@ -153,7 +153,7 @@ public class Player extends Entity {
         animations = new BufferedImage[9][6];
         for (int j = 0; j < animations.length; j++)
             for (int i = 0; i < animations[j].length; i++)
-                animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
+                animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 124);
 
     }
 
