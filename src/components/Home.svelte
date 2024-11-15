371d93b8e0457d09ae9a9dfd64d1c94405110550
Eli <ebmaffry@gmail.com>
2024-11-14 20:46:06 -0500

    added timetable component/mock data functionality

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -74,50 +74,9 @@
 </script>
 
 <main class="grid-container">
-    <!-- Form -->
-    <div class="userInputBackground">
-        <form action="">
-            <input type="text" id="start" name="start" value="Start" /><br /><br />
-            <input type="text" id="end" name="end" value="End" />
-            <input type="submit" value="Swap" /><br /><br />
-            <div class="inlineElements">
-                <div class="pairLabel">
-                    <input type="radio" id="leave" name="button" value="leave" checked />
-                    <label for="leave">Leave</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="radio" id="arrive" name="button" value="arrive" />
-                    <label for="arrive">Arrive</label>
-                </div>
-                <a href="">+Round Trip</a>
-                <a href="">+Add Stop</a>
-            </div>
-            <br />
-            <input type="text" id="date" name="date" value="Today" /><br /><br />
-            <input type="text" id="time" name="time" value="ASAP" /><br /><br />
-            <div class="inlineElements">
-                <div class="pairLabel">
-                    <input type="checkbox" id="bus" name="bus" checked />
-                    <label for="bus">Bus</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="checkbox" id="subway" name="subway" checked />
-                    <label for="subway">Subway</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="checkbox" id="trolley" name="trolley" checked /><br />
-                    <label for="trolley">Trolley</label>
-                </div>
-                <div class="pairLabel">
-                    <input type="checkbox" id="rail" name="rail" checked />
-                    <label for="rail">Rail</label>
-                </div>
-            </div>
-            <br />
-            <input type="button" id="fetch-button" value="Fetch Locations" />
-        </form>
-    </div>
-
+     <!-- Form -->
+     <TripPlanner/>
+     <input type="button" id="fetch-button" value="Fetch Locations" />
     <!-- Map -->
     <div class="map-container">
         <div id="map" style="height: 500px; width: 100%;"></div>
