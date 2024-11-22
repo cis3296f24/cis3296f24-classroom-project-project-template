287d6ff4a8a203055126255f1c5264e013340e33
Jiacheng He <tuo44998@temple.edu>
2024-11-21 22:28:02 -0500

    modified some styling

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,15 +1,15 @@
 function Comment() {
   return (
-    <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased rounded-lg">
+    <section class="bg-gray-900 py-8 lg:py-16 antialiased rounded-lg">
       <div class="max-w-2xl mx-auto px-4">
         <div class="flex justify-between items-center mb-6">
           <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Comments (3)</h2>
         </div>
         <form class="mb-6">
-          <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+          <div class="py-2 px-4 mb-4 rounded-lg rounded-t-lg border bg-gray-800 border-gray-700">
             <label for="comment" class="sr-only">Your comment</label>
             <textarea id="comment" rows="6"
-              class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+              class="px-0 w-full text-sm border-0 focus:ring-0 focus:outline-none text-white placeholder-gray-400 bg-gray-800"
               placeholder="Write a comment..." required></textarea>
           </div>
           <div class="px-8 py-32">
@@ -23,10 +23,10 @@ function Comment() {
             </div>
           </div>
         </form>
-        <article class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+        <article class="p-6 text-base rounded-lg bg-gray-900">
           <footer class="flex justify-between items-center mb-2">
             <div class="flex items-center">
-              <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+              <p class="inline-flex items-center mr-3 text-sm text-white font-semibold">
                 <img
                   class="mr-2 w-6 h-6 rounded-full"
                   src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
@@ -34,17 +34,17 @@ function Comment() {
                 />
                 Michael Gough
               </p>
-              <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
+              <p class="text-sm text-gray-400"><time pubdate datetime="2022-02-08"
                 title="February 8th, 2022">Nov. 8, 2024</time></p>
             </div>
           </footer>
-          <p class="text-gray-500 dark:text-gray-400">Epic Profile bro</p>
+          <p class="text-gray-400">Epic Profile bro</p>
         </article>
         
-        <article class="p-6 mb-3 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+        <article class="p-6 mb-3 text-base border-t border-gray-200 border-gray-700 bg-gray-900">
           <footer class="flex justify-between items-center mb-2">
             <div class="flex items-center">
-              <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+              <p class="inline-flex items-center mr-3 text-sm text-white font-semibold">
                 <img
                   class="mr-2 w-6 h-6 rounded-full"
                   src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
@@ -52,16 +52,16 @@ function Comment() {
                 />
                 Bonnie Green
               </p>
-              <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-03-12"
+              <p class="text-sm text-gray-400"><time pubdate datetime="2022-03-12"
                 title="March 12th, 2022">Mar. 12, 2024</time></p>
             </div>
           </footer>
-          <p class="text-gray-500 dark:text-gray-400">Sick profile man you are a grinder for real</p>
+          <p class="text-gray-400">Sick profile man you are a grinder for real</p>
         </article>
-        <article class="p-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+        <article class="p-6 text-base border-t border-gray-200 border-gray-700 bg-gray-900">
           <footer class="flex justify-between items-center mb-2">
             <div class="flex items-center">
-              <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+              <p class="inline-flex items-center mr-3 text-sm text-white font-semibold">
                 <img
                   class="mr-2 w-6 h-6 rounded-full"
                   src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
@@ -69,11 +69,11 @@ function Comment() {
                 />
                 Helene Engels
               </p>
-              <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-06-23"
+              <p class="text-sm text-gray-400"><time pubdate datetime="2022-06-23"
                 title="June 23rd, 2022">Nov. 12, 2024</time></p>
             </div>
           </footer>
-          <p class="text-gray-500 dark:text-gray-400">Keep up the grinding man.</p>
+          <p class="text-gray-400">Keep up the grinding man.</p>
         </article>
       </div>
     </section>
