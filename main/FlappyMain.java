53ccaea35290157fda540ee674bf4b1d4f14482f
Shafiq Rahman <shafiq@temple.edu>
2024-12-04 12:53:16 -0500

    Cleaning up code and adding descriptions.

--- main/FlappyMain.java
+++ main/FlappyMain.java
@@ -1,12 +1,18 @@
 package main;
 
-// Flappy Bird Game
-// Main program to execute the game Flappy Bird
-// This project is divided into multiple packages for easy debugging.
+/**
+ * Main entry point for the Flappy Bird game.
+ * This class initiates the game by creating an instance of the FlappyGame class.
+ */
 public class FlappyMain {
 
+	/**
+	 * The main method to start the Flappy Bird game.
+	 *
+	 * @param args command-line arguments (not used in this application)
+	 */
 	public static void main(String[] args) {
+		// Create an instance of FlappyGame to start the game
 		new FlappyGame();
 	}
-
 }
--- main/GamePanel.java
+++ main/GamePanel.java
@@ -8,11 +8,19 @@ import inputs.MouseInputs;
 import static main.FlappyGame.GAME_HEIGHT;
 import static main.FlappyGame.GAME_WIDTH;
 
+/**
+ * GamePanel is a JPanel that renders the game's graphics and handles user inputs.
+ */
 public class GamePanel extends JPanel {
 
 	private MouseInputs mouseInputs;
 	private FlappyGame flappyGame;
 
+	/**
+	 * Constructs a GamePanel with the specified game instance.
+	 *
+	 * @param flappyGame The FlappyGame instance this panel is part of.
+	 */
 	public GamePanel(FlappyGame flappyGame) {
 		mouseInputs = new MouseInputs(this);
 		this.flappyGame = flappyGame;
@@ -22,22 +30,37 @@ public class GamePanel extends JPanel {
 		addMouseMotionListener(mouseInputs);
 	}
 
+	/**
+	 * Sets the preferred size of the panel based on the game's width and height.
+	 */
 	private void setPanelSize() {
 		Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
 		setPreferredSize(size);
 	}
 
+	/**
+	 * Updates the game state. Currently does not perform any operations.
+	 */
 	public void updateGame() {
-
+		// Method to update the game state
 	}
 
+	/**
+	 * Renders the game's graphics.
+	 *
+	 * @param g The Graphics object used to render the graphics.
+	 */
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		flappyGame.render(g);
 	}
 
+	/**
+	 * Returns the FlappyGame instance associated with this panel.
+	 *
+	 * @return The FlappyGame instance.
+	 */
 	public FlappyGame getGame() {
 		return flappyGame;
 	}
-
 }
